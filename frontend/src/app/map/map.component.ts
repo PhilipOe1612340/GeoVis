a72bd2fe8c79d4541629c9c8effd47e3861d8eb7
Philip Oesterlin <philip.oesterlin+github@gmail.com>
2020-11-26 09:11:15 +0100

    wip

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import { DataService, GeoObj, Properties } from '../services/data.service';
 import { environment } from 'src/environments/environment';
-import { GermanyJson } from './germay';
-
+import { countries } from './overlay';
 
 class Icon extends L.Icon<L.IconOptions>{
   constructor(prop: Properties) {
@@ -25,8 +24,7 @@ export class MapComponent implements OnInit {
   private map!: L.Map;
   private amenitiesLayer: L.LayerGroup<any> = L.layerGroup();
   private iconMap: WeakMap<Properties, Icon> = new WeakMap<Properties, Icon>()
-
-  // tslint:disable-next-line: variable-name
+  private highlighted: any = null;
   private amenities: GeoObj[] = [];
 
   constructor(dataService: DataService) {
@@ -41,37 +39,97 @@ export class MapComponent implements OnInit {
       return;
     }
 
+    const info = new L.Control();
+
+    const highlightFeature = (e: L.LeafletMouseEvent) => {
+      if (this.highlighted !== null) {
+        this.amenitiesLayer.resetStyle(this.highlighted);
+        this.highlighted = null;
+      }
+      var layer = e.target;
+      layer.setStyle(style);
+      if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+      }
+      // info.update(layer.feature.properties);
+      this.highlighted = e.target;
+    }
+
+    const resetHighlight = (e: L.LeafletMouseEvent) => {
+      this.amenitiesLayer.resetStyle(e.target);
+      // info.update();
+    }
+
+    // Statistics information panel
+    // info.onAdd = () => {
+    //   // info._div = L.DomUtil.create('div', 'info');
+    //   info.update();
+    //   // return this._div;
+    // };
+    // info.addTo(this.map);
+
     // remove old amenities
     this.map.removeLayer(this.amenitiesLayer);
 
     // @ts-ignore
     // this.amenitiesLayer = L.geoJSON({ "type": "FeatureCollection", features: this.amenities }, {
-    this.amenitiesLayer = L.geoJSON(GermanyJson, {
-      pointToLayer: (_feature, latLng) => {
-        return L.marker(latLng);
-      },
-      onEachFeature: (feature: GeoObj, layer) => {
-        if (feature.geometry.type === 'Polygon' && feature.properties.icon) {
-          let center: L.LatLngExpression = feature.properties.center!;
-          if (!center) {
-            // @ts-ignore
-            const bounds = layer.getBounds(); // Get bounds of polygon
-            center = bounds.getCenter(); // Get center of bounds
-          }
+    this.amenitiesLayer = L.geoJSON(countries, {
+      clickable: true,
+      // pointToLayer: (feature, latLng) => {
+      //   return L.marker(latLng).bindPopup(feature.properties.name).openPopup();
+      // },
+      // onEachFeature: (feature: GeoObj, layer) => {
+      //   if (feature.geometry.type === 'Polygon' && feature.properties.icon) {
+      //     let center: L.LatLngExpression = feature.properties.center!;
+      //     if (!center) {
+      //       // @ts-ignore
+      //       const bounds = layer.getBounds(); // Get bounds of polygon
+      //       center = bounds.getCenter(); // Get center of bounds
+      //     }
 
-          // Use center to put marker on map
-          let icon;
-          if (this.iconMap.has(feature.properties)) {
-            icon = this.iconMap.get(feature.properties);
-          } else {
-            icon = new Icon(feature.properties);
-            this.iconMap.set(feature.properties, icon);
-          }
+      //     // Use center to put marker on map
+      //     let icon;
+      //     if (this.iconMap.has(feature.properties)) {
+      //       icon = this.iconMap.get(feature.properties);
+      //     } else {
+      //       icon = new Icon(feature.properties);
+      //       this.iconMap.set(feature.properties, icon);
+      //     }
 
-          L.marker(center, { icon }).addTo(this.map);
-        }
+      //     L.marker(center, { icon })
+      //     var popup = L.popup()
+      //       .setLatLng(center)
+      //       .setContent(feature.properties.name)
+      //       .openOn(this.map);
+      //     // .bindPopup().openPopup(center).addTo(this.map);
+      //   }
+      // }
+
+      onEachFeature: (_feature, layer) => {
+        layer.on({
+          mouseover: highlightFeature,
+          mouseout: resetHighlight,
+          click: highlightFeature,
+        });
       }
     });
+
+    function getColor() {
+      return "#ff0000";
+    }
+
+    const style = () => {
+      return {
+        stroke: true,
+        fill: true,
+        fillColor: getColor(),
+        weight: 0.5,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+      };
+    }
 
     // this.amenitiesLayer.addTo(this.map);
     this.map.addLayer(this.amenitiesLayer);
@@ -86,30 +144,11 @@ export class MapComponent implements OnInit {
    * Often divs and other HTML element are not available in the constructor. Thus we use onInit()
    */
   ngOnInit(): void {
-    // some settings for a nice shadows, etc.
-    const iconRetinaUrl = './assets/marker-icon-2x.png';
-    const iconUrl = './assets/marker-icon.png';
-    const shadowUrl = './assets/marker-shadow.png';
-    const iconDefault = L.icon({
-      iconRetinaUrl,
-      iconUrl,
-      shadowUrl,
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-      tooltipAnchor: [16, -28],
-      shadowSize: [41, 41],
-    });
-
-    L.Marker.prototype.options.icon = iconDefault;
-
     // basic setup, create a map in the div with the id "map"
-    this.map = L.map('map').setView([47.66, 9.175], 2);
+    this.map = L.map('map', { crs: L.CRS.Simple }).setView([47.66, 9.175], 2);
 
-    // set a tilelayer, e.g. a world map in the background
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(this.map);
+    const bounds: L.LatLngBoundsExpression = [[-180, -180], [180, 180]];
+    const image = L.imageOverlay('./assets/black.svg', bounds).addTo(this.map);
   }
 
   /**
